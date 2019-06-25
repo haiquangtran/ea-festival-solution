afe4383b8ef03cbe84f811e3ccc2af5f68e449f2
Hai Tran <hai.quang.tran@gmail.com>
2019-06-26 08:56:38 +1000

    setup project architecture for solution

---
+++ src/EA.Festival.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EA.Festival.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
