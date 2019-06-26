67024e306bf09a48e167ae0e19b8de7c2a49e2da
Hai Tran <hai.quang.tran@gmail.com>
2019-06-26 18:42:23 +1000

    added FestivalDataService scaffold.

---
+++ src/EA.Festival.Domain/Interfaces/IFestivalDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EA.Festival.Domain.Models;
+
+namespace EA.Festival.Domain.Interfaces
+{
+    public interface IFestivalDataService
+    {
+        IEnumerable<MusicFestival> GetMusicFestivals();
+    }
+}
---
+++ src/EA.Festival.Infra.Data/Services/FestivalDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EA.Festival.Domain.Interfaces;
+using EA.Festival.Domain.Models;
+
+namespace EA.Festival.Infra.Data.Services
+{
+    public class FestivalDataService : IFestivalDataService
+    {
+        public IEnumerable<MusicFestival> GetMusicFestivals()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
